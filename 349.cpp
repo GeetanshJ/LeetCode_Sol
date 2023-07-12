e04d36bb449d483f7fa074f8e787a8f7691988bd
GeetanshJ <geetansh0513.be21@chitkara.edu.in>
2023-07-12 13:02:55 +0530

    sol

---
+++ 349.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> ans;
+
+        for(int i = 0;i < nums1.size(); i++){
+            for(int j = i + 1;j < nums1.size(); j++){
+                if(nums1[i] == nums1[j]){
+                    nums1[j] = INT_MAX;
+                }
+            }
+        }
+
+        for(int i = 0;i < nums1.size(); i++){
+            for(int j = 0;j < nums2.size(); j++){
+                if(i != INT_MAX && nums1[i] == nums2[j]){
+                    ans.push_back(nums1[i]);
+                    nums1[i] = INT_MAX;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
