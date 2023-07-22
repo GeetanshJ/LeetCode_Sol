e3669db0134fe2b30779f62c6e5c402991e29393
GeetanshJ <geetansh0513.be21@chitkara.edu.in>
2023-07-22 21:49:04 +0530

    sol

---
+++ 2778.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int sumOfSquares(vector<int>& nums) {
+        int sum = 0;
+
+        for(int i = 0;i < nums.size(); i++){
+            if(nums.size() % (i+1) == 0){
+                sum = sum + nums[i]*nums[i];
+            }
+        }
+
+        return sum;
+    }
+};
