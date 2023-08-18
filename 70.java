3844b0a76b08a68f1f94f74ed407f406f279842e
GeetanshJ <geetansh0513.be21@chitkara.edu.in>
2023-08-18 21:02:10 +0530

    sol

---
+++ 70.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int climbStairs(int n) {
+
+        if(n == 0 || n == 1){
+            return 1;
+        }
+
+        if(n == 2){
+            return 2;
+        }
+
+        return climbStairs(n-1) + climbStairs(n-2);
+    }
+}
