855a2f3edabd42aba1fb593583a7140e9b728b41
GeetanshJ <geetansh0513.be21@chitkara.edu.in>
2023-08-22 20:29:46 +0530

    sol

---
+++ 168.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        string ans;
+        
+        while(columnNumber > 0) {
+            columnNumber--;
+            ans += (char)(columnNumber % 26 + 'A');
+            columnNumber /= 26;
+        }
+        
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
