8f8a6b0a8459e4970c3d6bdaf85cbd8cc349e258
GeetanshJ <geetansh0513.be21@chitkara.edu.in>
2023-09-04 20:02:16 +0530

    sol

---
+++ 944.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+        
+        int answer = 0;
+        for (int col = 0; col < strs[0].size(); col++) {
+            for (int row = 1; row < strs.size(); row++) {
+                if (strs[row][col] < strs[row - 1][col]) {
+                    answer++;
+                    break;
+                }
+            }
+        }
+        
+        return answer;
+    }
+};
