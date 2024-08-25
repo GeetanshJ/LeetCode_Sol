352fd62236ab6ef2a7868c6e141554aa33643764
GeetanshJ <geetansh0513.be21@chitkara.edu.in>
2024-08-25 18:27:43 +0530

    sol

---
+++ 145.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void solve(TreeNode* root, vector<int>& arr){
+        if(root == nullptr) return;
+
+        solve(root->left,arr);
+        solve(root->right,arr);
+        arr.push_back(root->val);
+    }
+
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int>arr;
+        solve(root,arr);
+
+        return arr;
+    }
+};
