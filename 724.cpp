bf2580fefd1a11468fbe06e3a37d8e64ed071344
GeetanshJ <geetansh0513.be21@chitkara.edu.in>
2024-09-21 20:26:49 +0530

    sol

---
+++ 724.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+class Solution {
+public:
+    int pivotIndex(std::vector<int>& nums) {
+        int total_sum = 0;
+        int left_sum = 0;
+        for (int num : nums) {
+            total_sum += num;
+        }
+        
+        for (int i = 0; i < nums.size(); ++i) {
+            int right_sum = total_sum - left_sum - nums[i];
+            
+            if (left_sum == right_sum) {
+                return i;
+            }
+            
+            left_sum += nums[i];
+        }
+        return -1;
+    }
+};
